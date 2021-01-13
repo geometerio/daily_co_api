ef2354756f97cc9f9c7b24c4c7123bd8cf7243df
Greg Woodward <woodward@alumni.stanford.edu>
2021-01-13 07:22:47 -0800

    MeetingToken.validate(meeting_token) works.

--- lib/daily_co_api/meeting_token.ex
+++ lib/daily_co_api/meeting_token.ex
@@ -11,13 +11,36 @@ defmodule DailyCoAPI.MeetingToken do
     end
   end
 
-  defp create_meeting_token_url(), do: HTTP.daily_co_api_endpoint() <> "meeting-tokens"
+  def validate(meeting_token) do
+    {:ok, http_response} = HTTP.client().get(validate_meeting_token_url(meeting_token), HTTP.headers())
 
+    case http_response do
+      %{status_code: 200, body: json_response} -> {:ok, json_response |> Jason.decode!() |> validated_meeting_token()}
+      %{status_code: 400} -> {:error, :invalid_meeting_token}
+      %{status_code: 401} -> {:error, :unauthorized}
+    end
+  end
+
+  defp create_meeting_token_url(), do: HTTP.daily_co_api_endpoint() <> "meeting-tokens"
+  defp validate_meeting_token_url(meeting_token), do: HTTP.daily_co_api_endpoint() <> "meeting-tokens/#{meeting_token}"
+
+  # TODO: combine these two functions!!!
   defp extract_meeting_token(meeting_token_json) do
     %{
       close_tab_on_exit: meeting_token_json["close_tab_on_exit"],
       enable_recording: meeting_token_json["enable_recording"],
       exp: meeting_token_json["exp"],
+      is_owner: meeting_token_json["is_owner"],
+      room_name: meeting_token_json["room_name"],
+      start_video_off: meeting_token_json["start_video_off"],
+      user_name: meeting_token_json["user_name"]
+    }
+  end
+
+  defp validated_meeting_token(meeting_token_json) do
+    %{
+      lang: meeting_token_json["lang"],
+      start_audio_off: meeting_token_json["start_audio_off"],
       is_owner: meeting_token_json["is_owner"],
       room_name: meeting_token_json["room_name"],
       start_video_off: meeting_token_json["start_video_off"],
--- test/daily_co_api/meeting_token_test.exs
+++ test/daily_co_api/meeting_token_test.exs
@@ -61,4 +61,50 @@ defmodule DailyCoAPI.MeetingTokenTest do
       }
     end
   end
+
+  describe "validate/1" do
+    test "success" do
+      expect(HTTPoisonMock, :get, fn url, headers ->
+        assert url == "https://api.daily.co/v1/meeting-tokens/valid-meeting-token"
+        assert_correct_headers(headers)
+        json_response = File.read!("test/daily_co_api/validated_meeting_token_response.json")
+        {:ok, %HTTPoison.Response{status_code: 200, body: json_response}}
+      end)
+
+      {:ok, meeting_token} = MeetingToken.validate("valid-meeting-token")
+      assert meeting_token == expected_valid_meeting_token()
+    end
+
+    test "gives an error if the meeting token is invalid" do
+      expect(HTTPoisonMock, :get, fn url, headers ->
+        assert url == "https://api.daily.co/v1/meeting-tokens/invalid-meeting-token"
+        assert_correct_headers(headers)
+        {:ok, %HTTPoison.Response{status_code: 400, body: "{}"}}
+      end)
+
+      response = MeetingToken.validate("invalid-meeting-token")
+      assert response == {:error, :invalid_meeting_token}
+    end
+
+    test "unauthorized" do
+      expect(HTTPoisonMock, :get, fn url, headers ->
+        assert url == "https://api.daily.co/v1/meeting-tokens/valid-meeting-token"
+        assert_correct_headers(headers)
+        {:ok, %HTTPoison.Response{status_code: 401, body: ""}}
+      end)
+
+      {:error, :unauthorized} = MeetingToken.validate("valid-meeting-token")
+    end
+
+    defp expected_valid_meeting_token() do
+      %{
+        room_name: "EevVrrxee4JBxKzmKkjC",
+        is_owner: true,
+        user_name: "host",
+        start_video_off: false,
+        start_audio_off: true,
+        lang: "en"
+      }
+    end
+  end
 end
